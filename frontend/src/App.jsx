bef131b7cfddbfcad65b44d80e7260c7d6da5654
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-04-05 15:18:54 +0530

    updated app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 
 import React from 'react'
 import Routings from './PageRouting/Routings'
-import AnalyzeResume from './pages/AnalyzeResume' 
-import JobRecommendations from './pages/JobRecommendations'
-import LandingPage from './pages/LandingPage'
+
 
 function App() {
   return (
